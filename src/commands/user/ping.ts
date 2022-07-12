f1adf56b0035365a5a7154cc225e04cba2d4ede8
tritranduc11 <tritranduc11@gmail.com>
2022-07-12 21:10:45 +0700

    add message and slash handlers
    Co-authored-by: conghoangdev <conghoangdev@users.noreply.github.com>

---
+++ src/commands/user/ping.ts
@@ -0,0 +1,15 @@
+import { Message } from "discord.js";
+import { callbackFunc, ICommand } from "helper-package-create-discord-bot";
+import { TMetaData } from "../../types/MetaData";
+
+export default {
+  name: "ping",
+  description: "Ping",
+  category: "user",
+  aliases: ["p"],
+  isSlash: true,
+  callback: async ({ client }) => {
+    return "Pong! ping : " + client.ws.ping + "ms";
+  },
+
+} as ICommand<TMetaData>;
--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,30 @@ import { Client } from "discord.js";
 import dotenv from "dotenv";
 import path from "path";
 import { allIntents } from "./constants";
+import { Command } from "helper-package-create-discord-bot";
+import { TMetaData } from "./types/MetaData";
 dotenv.config({
   path: path.join(__dirname, "..", ".env"),
 });
+
+const isDev = process.env.NODE_ENV !== "production";
 
 const client = new Client({
   intents: allIntents,
 });
 
-client.on("ready", (client) => {
+const command = new Command<TMetaData>(client, {
+  commandDir: path.join(__dirname, "./commands"),
+  owner: ["889140130105929769", "972383714166321232"],
+  isDev,
+  LogForMessageAndInteraction: isDev,
+  typescript: isDev,
+  metaData: {},
+  BotPrefix: "!",
+});
+
+client.on("ready", async(client) => {
+  await command.init()
   console.log(`login as ${client.user.username}`);
 });
 
